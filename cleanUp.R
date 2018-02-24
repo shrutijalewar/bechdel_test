184349008296ef35b6fc4d2c78c87c2edbe4139e
Shruti Sharma <shruti.sharma@ingramcontent.com>
2018-02-24 14:12:07 -0600

    clean data uploaded

--- cleanUp.R
+++ cleanUp.R
@@ -6,19 +6,24 @@ library("dplyr")
 library("magrittr")
 library("ggplot2")
 library("readxl")
+library("ggvis")
+library("gender")
+library("genderdata")
 
 # Reading in data from bechdel api and imdb
 bechdel_scores <- read_json('rawData/bechdel_scores', simplifyVector = TRUE)
-View(bechdel_scores)
 ratings <- read_tsv('rawData/title.ratings.tsv')
 name <- read_tsv('rawData/name.basics.tsv')
 crew <- read_tsv('rawData/title.crew.tsv')
-principals <- read_tsv('rawData/title.principals.tsv')
+# principals <- read_tsv('rawData/title.principals.tsv')
+title <- read_tsv('rawData/title.basics.tsv')
+# https://pudding.cool/2017/03/bechdel/#methodology
 
 # editing the bechdelIds to work with imdb_ids
 bechdel_scores$tconst <- paste('tt',bechdel_scores$imdbid, sep = '')
 bechdel_merge <- merge(bechdel_scores, crew, by='tconst')
 bechdel_merge <- merge(bechdel_merge, ratings, by='tconst')
+bechdel_merge <- merge(bechdel_merge, title, by='tconst')
 
 # creating a df with unique_nmIds
 # unique_nm <- distinct(bechdel_merge,directors,writers)
@@ -30,8 +35,7 @@ bechdel_merge2$birthYear <- as.numeric(bechdel_merge2$birthYear)
 # install.packages("gender")
 # install.packages("genderdata", type = "source",
 #                  repos = "http://packages.ropensci.org")
-library(gender)
-library(genderdata)
+
 data(package = "genderdata")
 gender(c("Madison", "Hillary"), years = 2000, method = "ssa")
 
@@ -47,6 +51,12 @@ str(gender_pred)
 
 # Merging the gender into the main file
 bechdel_merge3 <- merge(bechdel_merge2, gender_pred, by= 'Firstname')
+glimpse(bechdel_merge3)
+bechdel <- select(bechdel_merge3, tconst, nconst, rating, id, title, year, averageRating, numVotes, titleType, primaryTitle, originalTitle, 
+                  isAdult, runtimeMinutes, genres, Firstname, Lastname, gender)
+write_tsv(bechdel_merge3, 'data/bechdel_merge.tsv')
+write_tsv(bechdel, 'data/bechdel.tsv')
+bechdel_merge3 <- read_tsv('data/bechdel_merge.tsv')
 rating_bechdel <- bechdel_merge3 %>%
     group_by(rating,gender) %>%
     count()
@@ -62,4 +72,72 @@ female_bechdel <- bechdel_merge3 %>%
     count()
 rating_bechdel <- as.data.frame(rating_bechdel)
 
-    plot(rating_bechdel)
+bechdel_merge3$year <- as.numeric(bechdel_merge3$year) 
+
+bechdel_f <- bechdel_merge3 %>% 
+  filter(gender == 'female')
+bechdel_m <- bechdel_merge3 %>% 
+  filter(gender == 'male')
+# https://austinwehrwein.com/post/bechdel/
+
+ggplot(bechdel_merge3,aes(x=year))+
+  geom_point(stat='count',aes(color=rating))+
+  geom_line(stat='count',aes(color=rating))+
+  # a_robot_theme()+
+  # a_step_color()+
+  theme(legend.position = 'right')+
+  labs(title='Bechdel test scores over the years',
+       subtitle='Count of Bechdel test film scores.',
+       caption="Data from http://bechdeltest.com")
+
+ggplot(bechdel_m,aes(x=year))+
+  geom_point(stat='count',aes(color=rating))+
+  geom_line(stat='count',aes(color=rating))+
+  # a_robot_theme()+
+  # a_step_color()+
+  theme(legend.position = 'right')+
+  labs(title='Bechdel test scores over the years with male directors',
+       subtitle='Count of Bechdel test film scores.',
+       caption="Data from http://bechdeltest.com")
+
+ggplot(bechdel_f,aes(x=year))+
+  geom_point(stat='count',aes(color=rating))+
+  geom_line(stat='count',aes(color=rating))+
+  # a_robot_theme()+
+  # a_step_color()+
+  theme(legend.position = 'right')+
+  labs(title='Bechdel test scores over the years with female directors',
+       subtitle='Count of Bechdel test film scores.',
+       caption="Data from http://bechdeltest.com")
+
+bechdel_merge3 %>%
+  ggvis( ~ year, ~ averageRating) %>% 
+  layer_points(size = 50, size.hover = 200,
+               fillOpacity = 0.2, fillOpacity.hover = 0.5)
+  
+  # layer_points(size = 50, size.hover = 200,
+  #              fillOpacity = 0.2, fillOpacity.hover = 0.5,
+  #              stroke = as.factor(bechdel_merge3$gender), key = ~tconst) 
+  # add_tooltip(bechdel_merge3$title, "hover") 
+  # add_axis("x", title = rating) %>%
+  # add_axis("y", title = year) %>%
+  # add_legend("stroke", title = "gender:", values = c("male", "female")) 
+  # scale_nominal("stroke", domain = c("male", "female"),
+  #               range = c("orange", "#aaa")) 
+  # set_options(width = 500, height = 500)
+
+ui <- fluidPage(
+  ggvisOutput("plot")
+)
+
+server <- function(input, output) {
+  
+  bechdel_merge3 %>%
+    ggvis( ~ year, ~ averageRating) %>% 
+    layer_points() %>% 
+    
+    add_tooltip(function(bechdel_merge3) { paste0("title: ", title) }) %>%
+     bind_shiny("plot")
+}
+
+shinyApp(ui = ui, server = server)
