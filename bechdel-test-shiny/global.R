e8ad9fcc1eac89aa1330c83e157b99e4a0ba7be8
Shruti Sharma <shruti.sharma@ingramcontent.com>
2018-03-04 23:38:18 -0600

    data  updated

---
+++ bechdel-test-shiny/global.R
@@ -0,0 +1,9 @@
+# Variables that can be put on the x and y axes
+axis_vars <- c(
+  "Number of Votes" = "numVotes",
+  "Year Released"  = "year",
+  "Average imdb Rating" = "averageRating",
+  "Runtime (mins)" = "runtimeMinutes",
+  "Budget (US$)" = "budget",
+  "Revenue (US$)" = "revenue"
+)
